d86a6b89ffab5ea9a1dc59c5a2cff5d3661eac8f
Stephen Valdinger <stephen@chocolatey.io>
2026-05-06 17:40:34 -0400

    Create dynamic table example
    Adds a new App example to highlight using the New-DynamicTable
    function

---
+++ EXAMPLES/DynamicTable.ps1
@@ -0,0 +1,22 @@
+New-UDApp -Title 'Dynamic Table Demo' -Content { 
+
+    $newDynamicTableSplat = @{
+        Id      = 'process-table'
+        Data    = {
+            Get-Process | Select-Object -First 50 Name, Id, CPU, WorkingSet
+        }
+        Columns = {
+            New-UDTableColumn -Property Name -Title 'Name' -ShowSort
+            New-UDTableColumn -Property Id -Title 'PID'-ShowSort
+            New-UDTableColumn -Property CPU -Title 'CPU (s)' -ShowSort -Render {
+                '{0:N2}' -f [double]$EventData.CPU
+            }
+            New-UDTableColumn -Property WorkingSet -Title 'Memory (MB)' -ShowSort -Render {
+                '{0:N0}' -f ($EventData.WorkingSet / 1MB)
+            }
+        }
+    }
+
+    New-DynamicTable @newDynamicTableSplat
+
+}
